c34218e922226282485da957b630cf9cc59b9c4d
Corey Alix <coreyalix@gmail.com>
2023-01-03 14:51:43 -0500

    Auto-generated commit [ci skip]

--- ng-101/src/app/dictionary/meta-route/meta-route.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-meta-route',
-  templateUrl: './meta-route.component.html',
-  styleUrls: ['./meta-route.component.css'],
-})
-export class MetaRouteComponent {}
