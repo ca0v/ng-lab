ecb30bdab3846fc1890384e26ed00c70d4a95bef
Corey Alix <coreyalix@gmail.com>
2023-01-03 07:49:45 -0500

    Adds a binding to the form

--- ng-101/src/app/search-input-form/search-input-form.component.ts
+++ ng-101/src/app/search-input-form/search-input-form.component.ts
@@ -4,12 +4,17 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-search-input-form',
   templateUrl: './search-input-form.component.html',
-  styleUrls: ['./search-input-form.component.css'],  
+  styleUrls: ['./search-input-form.component.css'],
 })
 export class SearchInputFormComponent {
+  searchInputControl = new FormControl('');
+  searchTerm = '';
+
   search() {
     throw new Error('Method not implemented.');
   }
-  searchInputControl = new FormControl('');
-  searchTerm = '';
+
+  ngOnChanges() {
+    this.searchTerm = this.searchInputControl.value || '';
+  }
 }
