e46f3a5fbe2601da3b8fe3f22667ab33381786d2
Corey Alix <coreyalix@gmail.com>
2023-01-12 13:54:06 -0500

    Adding file counts, useful cli tool

--- ng-tauri/tauri-app/rust-lab/src/main.rs
+++ ng-tauri/tauri-app/rust-lab/src/main.rs
@@ -3,36 +3,83 @@
  * I created this rust-lab project to develop rust code.
  * Specifically, I want to discover the folder sizes on a local drive.
  * Those library methods will be developed here, and ported to the tauri app.
+ * Banner width 40
+ * ************ PARENT FOLDER ************
+ * 012345678901 0123456789012 012345678901
  */
 use std::fs;
 
 fn main() {
-    let folder = "/home/ca0v/code";
+    // read the 1st input argument
+    let args: Vec<String> = std::env::args().collect();
 
-    println!("FOLDERS");
+    // if no input argument, exit
+    if args.len() < 2 {
+        println!("Please provide a folder path as the 1st argument.");
+        return;
+    }
+
+    let folder = &args[1];
+    discover(folder);
+}
+
+fn discover(folder: &str) {
+    // confirm directory exists
+    if !std::path::Path::new(folder).exists() {
+        println!("'{}' does not exist.", folder);
+        return;
+    }
+
+    banner("PARENT FOLDER");
+    println!("{}", get_parent_folder(folder));
+
+    banner("CHILD FOLDERS");
     let mut folders = list_all_folders_in_folder(folder);
     folders.sort();
     for (_, folder) in folders.iter().enumerate() {
         println!(
-            "{:>6} {}",
+            "{:>6} {:60} {:>6} files",
             file_size_in_human_readable_format(get_folder_size(folder)),
             folder,
+            get_file_count(folder),
         );
     }
 
-    println!("FILES");
+    banner("FILES");
     let mut files = list_all_files_in_folder(folder);
     // sort files
     files.sort();
-    
+
     for (_, file) in files.iter().enumerate() {
         println!(
             "{:>6} {}",
             file_size_in_human_readable_format(get_file_size(file)),
             file,
         );
-
     }
+}
+
+fn banner(title: &str) {
+    let banner_size = 80;
+    let total_padding = banner_size - title.len() - 2;
+    let left_padding = total_padding / 2;
+    let right_padding = total_padding - left_padding;
+
+    // generate padding string
+    let mut left_pad = String::new();
+    for _ in 0..left_padding {
+        left_pad.push('*');
+    }
+
+    let mut right_pad = String::new();
+    for _ in 0..right_padding {
+        right_pad.push('*');
+    }
+
+    println!(
+        "\n{:*<width$}\n{} {} {}\n{:*<width$}",
+        "", left_pad, title, right_pad, "", width=banner_size
+    );
 }
 
 fn file_size_in_human_readable_format(size: u64) -> String {
@@ -61,40 +108,75 @@ fn file_size_in_human_readable_format(size: u64) -> String {
     return format!("{} {}", size, unit);
 }
 
+fn get_parent_folder(folder: &str) -> String {
+    let result = std::path::Path::new(&folder)
+        .parent()
+        .map(|p| p.to_str().unwrap().to_string());
+    return result.unwrap();
+}
+
 fn list_all_folders_in_folder(folder: &str) -> Vec<String> {
     // create a vector of strings as the return value
     let mut result: Vec<String> = Vec::new();
 
-    let paths = fs::read_dir(folder).unwrap();
-    for path in paths {
-        match path {
-            Ok(path) => {
-                if fs::metadata(path.path()).unwrap().is_dir() {
-                    result.push(path.path().to_str().unwrap().to_string());
+    let paths = fs::read_dir(folder);
+    match paths {
+        Ok(paths) => {
+            for path in paths {
+                match path {
+                    Ok(path) => {
+                        let meta = fs::metadata(path.path());
+                        match meta {
+                            Ok(meta) => {
+                                if meta.is_dir() {
+                                    result.push(path.path().to_str().unwrap().to_string());
+                                }
+                            }
+                            Err(e) => println!("{} {:?}", folder, e),
+                        }
+                    }
+                    Err(e) => println!("{} {:?}", folder, e),
                 }
             }
-            Err(e) => println!("{:?}", e),
+            return result;
+        }
+        Err(e) => {
+            println!("{} {:?}", folder, e);
+            return result;
         }
     }
-    return result;
 }
 
 fn list_all_files_in_folder(folder: &str) -> Vec<String> {
     // create a vector of strings as the return value
     let mut result: Vec<String> = Vec::new();
 
-    let paths = fs::read_dir(folder).unwrap();
-    for path in paths {
-        match path {
-            Ok(path) => {
-                if fs::metadata(path.path()).unwrap().is_file() {
-                    result.push(path.path().to_str().unwrap().to_string());
+    let paths = fs::read_dir(folder);
+    match paths {
+        Ok(paths) => {
+            for path in paths {
+                match path {
+                    Ok(path) => {
+                        let meta = fs::metadata(path.path());
+                        match meta {
+                            Ok(meta) => {
+                                if meta.is_file() {
+                                    result.push(path.path().to_str().unwrap().to_string());
+                                }
+                            }
+                            Err(e) => println!("{:?}", e),
+                        }
+                    }
+                    Err(e) => println!("{:?}", e),
                 }
             }
-            Err(e) => println!("{:?}", e),
+            return result;
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            return result;
         }
     }
-    return result;
 }
 
 fn get_file_size(file: &str) -> u64 {
@@ -111,6 +193,18 @@ fn get_folder_size(folder: &str) -> u64 {
     let folders = list_all_folders_in_folder(folder);
     for folder in folders {
         result += get_folder_size(&folder);
+    }
+    return result;
+}
+
+fn get_file_count(folder: &str) -> u16 {
+    let mut result: u16 = 0;
+    let files = list_all_files_in_folder(folder);
+    result += files.len() as u16;
+
+    let folders = list_all_folders_in_folder(folder);
+    for folder in folders {
+        result += get_file_count(&folder);
     }
     return result;
 }
